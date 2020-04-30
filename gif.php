ad03199a3417a05265ba1d3b78f69082518f2d22
jonathan-turner90 <64594122+jonathan-turner90@users.noreply.github.com>
2020-04-30 17:13:15 +0100

    Update gif.php

--- gif.php
+++ gif.php
@@ -59,11 +59,4 @@
 		$now->modify('+1 second');
 	}
 
-	//expire this image instantly
-	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
-	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
-	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
-	header( 'Cache-Control: post-check=0, pre-check=0', false );
-	header( 'Pragma: no-cache' );
-	$gif = new AnimatedGif($frames,$delays,$loops);
-	$gif->display();
+
