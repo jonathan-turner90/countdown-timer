b52500a0eae1088bbeefe68f2f628d806a97f25b
jonathan-turner90 <64594122+jonathan-turner90@users.noreply.github.com>
2020-04-30 15:30:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; $gif = new AnimatedGif($frames,$delays,$loops);
+	$gif->display();?> 
+  
  </body>
 </html>
