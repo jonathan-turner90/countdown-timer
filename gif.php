c32459c7638a413dca6c35cf3ee1fc3808233597
jonathan-turner90 <64594122+jonathan-turner90@users.noreply.github.com>
2020-05-01 13:58:47 +0100

    Update gif.php

--- gif.php
+++ gif.php
@@ -20,7 +20,7 @@
 	$font = array(
 		'size' => 50, // Font size, in pts usually.
 		'angle' => 0, // Angle of the text
-		'x-offset' => 60, // The larger the number the further the distance from the left hand side, 0 to align to the left.
+		'x-offset' => 70, // The larger the number the further the distance from the left hand side, 0 to align to the left.
 		'y-offset' => 50, // The vertical alignment, trial and error between 20 and 60.
 		'file' => __DIR__ . DIRECTORY_SEPARATOR . 'Futura.ttc', // Font path
 		'color' => imagecolorallocate($image, 255, 255, 255), // RGB Colour of the text
