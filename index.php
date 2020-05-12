86b97eb3c2072ed6f986deb899f00833599b38f2
jonathan-turner90 <64594122+jonathan-turner90@users.noreply.github.com>
2020-05-12 20:29:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	include 'GIFEncoder.class.php';
 	include 'php52-fix.php';
 	$time = $_GET['time'];
-	$date = '2020-05-05 13:04';
+	$date = '2020-05-15 13:04';
 	$future_date = new DateTime(date($date,strtotime($time)));
 	$time_now = time();
 	$now = new DateTime(date('r', $time_now));
@@ -108,7 +108,7 @@
 	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
 	header( 'Cache-Control: post-check=0, pre-check=0', false );
 	header( 'Pragma: no-cache' );
-	$gif = new \temp\AnimatedGif($frames,$delays,$loops);
-	return $gif->getAnimation();
+	$gif = new AnimatedGif($frames,$delays,$loops);
+	$gif->display();
 
 
